b495aec328d46224956818f40e28e439f7d080c1
SananNaghshbandi <sananqsh@gmail.com>
2021-09-04 21:45:54 +0430

    task 1 done

--- 1.py
+++ 1.py
@@ -1,33 +1,18 @@
 def middle_idx(word):
 	return int((len(word) - 1) / 2)
 
-def middle_char(word):
-	middle_idx = int((len(word) - 1) / 2)
-	return word[middle_idx]
-
-def without(word, idx):
-	return word[0:idx] + word[idx+1:len(word)]
-
-def swap(word):
-	if len(word) == 0:
-		return word
-
 def encrypt(word):
-	print(word)	#dbg
-
 	if len(word) <= 2:
 		return word;
 
 	mid_idx = middle_idx(word)
-	mid_char = middle_char(plain)		
+	mid_char = word[middle_idx(word)]
 	
 	word = mid_char + encrypt(word[0:mid_idx]) + encrypt(word[mid_idx+1:len(word)])
 
+	return word
 
-while True:
-	plain = input("Enter a word to have it ciphered: ")
-	print(plain)
+plain = input("Enter a word to have it ciphered: ")
 
-	# while 
-
-	print(encrypt(plain))
+cipher = encrypt(plain)
+print("Cipher text: " + cipher)
